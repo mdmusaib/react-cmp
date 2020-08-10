8d47c74d668f3198e2ae60a49cdc12401cacd0ac
Mohammed Musaib <mohammed.musaib@outlook.com>
2020-08-10 12:45:05 +0530

    Updates

---
+++ precache-manifest.ef2f2824274d53e2d54484e2484fae40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199518fa1885c4d97b586c2d466b0949",
+    "url": "/react-cmp/index.html"
+  },
+  {
+    "revision": "047fc38ff35e0d18bd32",
+    "url": "/react-cmp/static/css/2.43e9891d.chunk.css"
+  },
+  {
+    "revision": "a76cace632cf71545ab8",
+    "url": "/react-cmp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "047fc38ff35e0d18bd32",
+    "url": "/react-cmp/static/js/2.49a68d8f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-cmp/static/js/2.49a68d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a76cace632cf71545ab8",
+    "url": "/react-cmp/static/js/main.bb595629.chunk.js"
+  },
+  {
+    "revision": "87c9fdb0f2172d4fe4f5",
+    "url": "/react-cmp/static/js/runtime-main.0525996b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cmp/static/media/logo.5d5d9eef.svg"
+  }
+]);
